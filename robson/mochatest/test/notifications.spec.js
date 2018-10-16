a2cf9eb8c3e8378b697669e9e95c45c37c7c5bc1
RobsonChiambiro <msupioneers@gmail.com>
2018-10-16 19:59:03 +1300

    added a test and

---
+++ robson/mochatest/test/notifications.spec.js
@@ -0,0 +1,22 @@
+const assert  = require('chai').assert
+const sut = require('../../notifications-app');
+
+describe('notification app', function(){
+    // Arrange
+
+    it('should get team members', function(){
+        throw('Not implemented');
+    });
+
+    it('should allow each member to opt in to receive reminders', function(){
+        throw('Not implemented');
+    });
+
+    it('should be able to send reminders using slack', function(){
+        throw('Not implemented');
+    });
+
+    it('should not remind users who have already captured their happiness levels', function(){
+        throw('Not implemented');
+    });
+})
