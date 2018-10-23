0df23201cbb94c8dd0989161df03058b2db47efc
RobsonChiambiro <msupioneers@gmail.com>
2018-10-23 23:27:20 +1300

    sending notifications to slack

--- robson/notifications-app.js
+++ robson/notifications-app.js
@@ -1,4 +1,4 @@
-const Slack = require('node-slack');
+const Slack = require('slack-node');
 const cron = require('node-schedule');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,6 +10,7 @@ app.get('/', onRequest);//app to get request
 
 // Use actual slack direct channe
 var enrolments = GetEnrolments();
+var schedules = schedules || [];
 
 app.use(bodyParser.json());
 app.post('/schedule_notifications', function(request, response){
@@ -19,7 +20,7 @@ app.post('/schedule_notifications', function(request, response){
 	var userId = postData.id;
 	var userDetails = enrolments[userId];
 	
-	if(userDetails)
+	if(userDetails && !notificationsScheduled())
 	{
 		// Update with received user response
 		var userResponse = postData.userResponse;
@@ -28,7 +29,7 @@ app.post('/schedule_notifications', function(request, response){
 
 		if(userResponse === 'accepted')
 		{
-			console.log("User accepted, scheduler starting...");
+			console.log("User accepted!");
 			startScheduler(userDetails.slackTeamWebHook, userDetails.slackChannel, userDetails.userName);
 		}
 	}
@@ -38,6 +39,11 @@ app.post('/schedule_notifications', function(request, response){
 	// postData.userResponse === 'rejected'
 	console.log(postData.id);
 });
+
+// filtering for specific user's schedule - could hook on to user took survey event to cancel reminders
+function notificationsScheduled(userName){
+	return schedules.filter(s => s.id === userName).length > 0;
+}
 
 const hostname = '127.0.0.1';
 const portNumber = 8080;
@@ -51,12 +57,13 @@ function GetEnrolments() {
 
 	var user = {
 		slackDirectWebHook: 'https://hooks.slack.com/services/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',
-		slackTeamWebHook: 'https://hooks.slack.com/services/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',
-		slackChannel: 'sdm2018_team13',
+		slackTeamWebHook: 'https://hooks.slack.com/services/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',
+		slackChannel: '#howisitnotifications',
 		userName: 'Robson',
 		email: 'qhs8122@autuni.ac.nz',
 		mobile: '0064210593902',
-		lastNotification: notification
+		lastNotification: notification,
+		surveyTaken: false
 	};
 	var enrolments = { user1: user };
 	return enrolments;
@@ -70,18 +77,28 @@ function onRequest (req, res) {
 // Start the notification scheduler for the registered user identified by id from client call
 function startScheduler(slackWebHook, slackChannel, userName){
 	var rule = new cron.RecurrenceRule();
-	rule.hour = 0.00;
-	rule.minute = 0.10;
+	rule.dayOfWeek = [new cron.Range(1, 5)]; //starting Monday to Friday
+	rule.hour = [new cron.Range(7, 23)]; //range in hours per day
+	rule.minute = [0, 10,19,20,30,40,50]; // minutes 
 
-	cron.scheduleJob(rule, function() {
-		var slack = new lack(slackWebHook);
-		slack.send({
+	console.log("scheduler starting...");
+
+	var j = cron.scheduleJob(rule, function() {
+		var slack = new Slack();
+		slack.setWebhook(slackWebHook);
+
+		slack.webhook({
 			text: 'Answer your Happiness Survey!',
-			channel: slackChannel
-			//username: userName
+			channel: slackChannel,
+			username: userName
+		}, function(err, response){
+			console.log(new Date() + 'Notification sent to Slack channel.');
+			console.log(response);
 		});
-		console.log(new Date(), 'Notification sent to Slack channel.');
 	});
+
+	// Add scheduled job to collection to allow later cancelling and prevent duplicate schedules for same user
+	schedules.push({'id': userName, 'job': j});
 }
 
 var server = app.listen(portNumber, function() {
